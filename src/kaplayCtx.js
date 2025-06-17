ec8283173c114a5fec11dc689f748792cdf17dbe
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-06-16 23:30:22 -0400

    fix: change pixel density to 2 for better rendering resolution

--- src/kaplayCtx.js
+++ src/kaplayCtx.js
@@ -2,7 +2,7 @@ import kaplay from "kaplay";
 export default function makeKaplayCtx() {
   return kaplay({
     global: false,
-    pixelDensity: 1,
+    pixelDensity: 2,
     touchToMouse: true,
     debug: false,
     debugKey: "f1",
